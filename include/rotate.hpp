773ff58d9687549e1005467094696dbebb46de46
NKanoya <3397540942@qq.com>
2025-09-06 23:33:52 +0800

    feat: add rotate function
    - add basic rotate functions for different tetronimoes

---
+++ include/rotate.hpp
@@ -0,0 +1,24 @@
+///
+/// @file @c include/rotate.hpp
+///
+
+#ifndef TETRIS_ROTATE_HPP
+#define TETRIS_ROTATE_HPP
+
+#include <iostream>
+#include <array>
+
+struct Axis {
+    std::size_t x;
+    std::size_t y;
+};
+
+using BlockShape = std::array<Axis, 4>;
+
+
+
+void eval_rotate_dest_I(const BlockShape& source, BlockShape& destination, bool clockwise = true);
+
+void eval_rotate_dest_normal(const BlockShape& source, BlockShape& destination, bool clockwise = true);
+
+#endif //TETRIS_ROTATE_HPP
---
+++ src/rotate/rotate.cpp
@@ -0,0 +1,54 @@
+///
+/// @file @c src/rotate/rotate.cpp
+///
+
+#include "../../include/rotate.hpp"
+
+
+
+void eval_rotate_dest_I(const BlockShape& source, BlockShape& destination, bool clockwise) {
+    if(&source == &destination){
+        // unsupported operation
+        // TODO: print warning info to log
+        return;
+    }
+
+    // rotate clockwise
+    if(clockwise) {
+        for(int i = 0; i < 4; ++i) {
+            destination[i].x = source[i].y;
+            destination[i].y = 3 - source[i].x;
+        }
+    }
+    // rotate counterclockwise
+    else {
+        for(int i = 0; i < 4; ++i) {
+            destination[i].x = 3 - source[i].y;
+            destination[i].y = source[i].x;
+        }
+    }
+}
+
+void eval_rotate_dest_normal(const BlockShape& source, BlockShape& destination, bool clockwise) {
+    if(&source == &destination){
+        // unsupported operation
+        // TODO: print warning info to log
+        return;
+    }
+
+    // rotate clockwise
+    if(clockwise) {
+        for(int i = 0; i < 4; ++i) {
+            destination[i].x = source[i].y;
+            destination[i].y = 2 - source[i].x;
+        }
+    }
+    // rotate counterclockwise
+    else {
+        for(int i = 0; i < 4; ++i) {
+            destination[i].x = 2 - source[i].y;
+            destination[i].y = source[i].x;
+        }
+    }
+}
+
