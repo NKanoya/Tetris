83d89ed6d36eb8b5a3dbf701cebde0c973ac92be
NKanoya <3397540942@qq.com>
2025-09-07 18:00:49 +0800

    test: make previous test function `test_block_map()` compatible to new block map structures

--- test/test_block_map/test_block_map.cpp
+++ test/test_block_map/test_block_map.cpp
@@ -5,31 +5,28 @@
 #include "../test_include.hpp"
 #include "../../include/map.hpp"
 
-void test_map() {
-    BlockMap bmap;
 
-    bmap.get_block(1,1) = 2;
-    bmap.get_block(1,2) = 2;
-    bmap.get_block(1,3) = 2;
-    bmap.get_block(2,1) = 2;
+namespace TetroTest {
+    void test_map() {
+        BlockMap bmap;
 
-    try {
-        for(int i = 0; i < block_map_properties.x_size; ++i) {
-            for(int j = 0; j < block_map_properties.y_size; ++j) {
-                if(bmap.get_block(i,j) == 0)
-                    printf("  ");
-                else
-                    printf("█ ");
+        bmap.get_block(1, 1) = TetrominoType::tetro_I;
+        bmap.get_block(1, 2) = TetrominoType::tetro_I;
+        bmap.get_block(1, 3) = TetrominoType::tetro_I;
+        bmap.get_block(2, 1) = TetrominoType::tetro_I;
+
+        try {
+            for (int i = 0; i < block_map_properties.x_size; ++i) {
+                for (int j = 0; j < block_map_properties.y_size; ++j) {
+                    if (bmap.get_block(i, j) == TetrominoType::empty)
+                        printf("  ");
+                    else
+                        printf("█▉");
+                }
+                printf("\n");
             }
-            printf("\n");
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
         }
-    } catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
     }
-
-
-
-
-
-
-};
+}
