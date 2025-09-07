55d266199ff29d31d9d17e3d5a0dbd5ecf395081
NKanoya <3397540942@qq.com>
2025-09-07 17:44:03 +0800

    feat: add `Tetromino` classes
    - add `Tetromino` classes capable to employ basic operations

---
+++ include/tetromino.hpp
@@ -0,0 +1,169 @@
+///
+/// @file @c src/tetromino.hpp
+///
+
+
+
+#ifndef TETRIS_TETROMINO_HPP
+#define TETRIS_TETROMINO_HPP
+
+#include <array>
+#include "map.hpp"
+#include "rotate.hpp"        // for type BlockShape
+#include "wall_kick.hpp"     // for enum class
+
+enum class TetrominoType: unsigned char {
+    empty,
+    tetro_O,
+    tetro_I,
+    tetro_T,
+    tetro_J,
+    tetro_L,
+    tetro_S,
+    tetro_Z
+};
+
+/**
+ * @brief Represents a tetromino, comprise of 4 blocks
+ *
+ * @ingroup @c RunningMap
+ */
+class Tetromino {
+protected:
+
+    TetrominoType m_type;
+
+    /*
+     * records 4 axises
+     * describes 4 blocks' position related in its 4x4 frame grid
+     * [0,0] is at the left-upper corner
+     *
+     * [0,0] [0,1] [0,2] [0,3]
+     * [1,0] [1,1] [1,2] [1,3]
+     * [2,0] [2,1] [2,2] [2,3]
+     * [3,0] [3,1] [3,2] [3,3]
+     */
+    BlockShape m_blocks;
+
+    // the absolute axis of its [0, 0] block within the block map
+    Axis m_axis;
+
+    // enum class RotateState
+    // recorded to employ specific wall-kick movement when the rotation is blocked
+    RotateState m_rotate_state;
+
+    // a weak pointer
+    // points to the block map that possesses the tetromino
+    BlockMap * m_blockmap;
+
+public:
+
+    /**
+     * construct a tetromino in a block map
+     *
+     * @ingroup a @c RunningMap class will store a tetromino with a @c std::unique_ptr
+     *
+     * @param type ( @c TetrominoType ):
+     * @param axis ( <code>const Axis&</code> ): the initial position in the map
+     * @param blockMap ( <code>const Axis&</code> ): a weak pointer to its block map
+     */
+    Tetromino(TetrominoType type, const Axis& axis, BlockMap * blockMap) noexcept;
+
+    // deleted copy constructor and assignment operator
+    Tetromino(const Tetromino& oth) = delete;
+    Tetromino& operator=(const Tetromino& oth) = delete;
+
+    // default moving constructor and assignment operator
+    Tetromino(Tetromino&& oth) = default;
+    Tetromino& operator=(Tetromino&& oth) = default;
+
+    /**
+     * @brief lets the tetromino fall (move downwards) 1 block
+     *
+     * <p>if the falling movement is blocked (bottoming out), it will notify its owner block
+     * to fix itself, and generate a new active tetromino.
+     */
+    void fall() noexcept;
+
+    /**
+     * @brief lets tetromino move 1 block leftwards
+     *
+     * <p>If the movement is blocked, the attempt will fail and the function will return.
+     */
+    void move_leftward() noexcept;
+
+    /**
+     * @brief lets tetromino move 1 block rightwards
+     *
+     * <p>If the movement is blocked, the attempt will fail and the function will return.
+     */
+    void move_rightward() noexcept;
+
+    /**
+     * @brief lets the tetromino rotate 90 degrees
+     *
+     * @param clockwise (@c bool , default: @c true ) the direction of rotation
+     *
+     * <p> The O, I, and other tetrominoes employ distinct rotation logic.
+     *
+     * @see the rotating logic is implemented in functions declared in @c rotate.hpp
+     *
+     * <p> if there's not enough room for the rotation, the function tries moves from the
+     * wall kick table
+     *
+     * <p> if all wall-kick moves fail (blocked), the rotation fails and the function returns.
+     *
+     * @see the wall-kick logic is implemented in functions declared in @c wall_kick.hpp
+     */
+    void rotate(bool clockwise = true) noexcept;
+
+protected:
+
+    /**
+     * @brief correctly changes rotate state after rotation
+     *
+     * @param rotate_state ( @c BlockState& ): the block state to be changed
+     * @param clockwise ( @c bool , default @c true ): the direction of rotation
+     *
+     * @see used in @c rotate()
+     */
+    static void change_rotate_state(RotateState& rotate_state, bool clockwise = true) noexcept;
+
+    /**
+     * @brief offsets a set of coordinates by a given displacement
+     *
+     * @param origin_axis ( @c Axis& ): the axis to be offset
+     * @param displacement ( <b>const Axis&</b> )
+     *
+     * @see used in @c rotate()
+     */
+    static void axis_displace(Axis& origin_axis, const Axis& displacement);
+
+    /**
+     * @brief employs the rotating operation
+     *
+     * <p>the operation includes changing coordinates of the blocks and switching rotation state
+     *
+     * <p>@b pure: the function won't conduct any checks or destination calculation, simply employing
+     * the rotating operation based on given destination
+     *
+     * @param destination ( <b>const BlockShape&</b> ): the destination
+     * @param clockwise ( @c bool , default @c true ): the rotating direction
+     */
+    void employ_rotation(const BlockShape& destination, bool clockwise = true) noexcept;
+
+    /**
+     * @brief checks if there's enough space for the wall kick operation
+     *
+     * check_space for the wall-kick movement
+     *
+     * @param origin_dest ( <b>const BlockShape&</b> ): the original destination
+     * @param wall_kick_disp ( <b>Axis</b> ): the wall-kick displacement
+     * @return ( @c bool ): if the wall-kick operation have enough space
+     */
+     bool check_space_for_wall_kick(const BlockShape& origin_dest, const Axis& wall_kick_disp) const noexcept;
+};
+
+static constexpr BlockShape get_shape(TetrominoType type) noexcept;
+
+#endif //TETRIS_TETROMINO_HPP
---
+++ src/tetromino/tetromino.cpp
@@ -0,0 +1,292 @@
+///
+/// @file @c src/tetromino/tetromino.cpp
+///
+
+#include "tetromino.hpp"
+
+static constexpr std::array<BlockShape, 7> tetromino_shape = {{
+      /*
+       * O Tetromino
+       * TetrominoType::tetro_O
+       *
+       * ··█▉█▉··
+       * ··█▉█▉··
+       * ········
+       * ········
+       */
+      {{{1,1},{0, 0}, {0, 1}, {1, 0}}},
+
+      /*
+       *
+       * I Tetromino
+       * TetrominoType::tetro_I
+       *
+       * ········
+       * █▉█▉█▉█▉
+       * ········
+       * ········
+       */
+      {{{1,1},{1, 0}, {1, 2}, {1, 3}}},
+
+      /*
+      * T Tetromino
+      * TetrominoType::tetro_T
+      *
+      * ··█▉····
+      * █▉█▉█▉··
+      * ········
+      * ········
+      */
+      {{{1,1},{0, 1}, {1, 0}, {1, 2}}},
+
+      /*
+       * J Tetromino
+       * TetrominoType::tetro_J
+       *
+       * █▉······
+       * █▉█▉█▉··
+       * ········
+       * ········
+       */
+      {{{1,1},{0, 0}, {1, 0}, {1, 2}}},
+
+      /*
+       * L Tetromino
+       * TetrominoType::tetro_L
+       *
+       * ····█▉··
+       * █▉█▉█▉··
+       * ········
+       * ········
+       */
+      {{{1,1},{0, 2}, {1, 0}, {1, 2}}},
+
+      /*
+       * S Tetromino
+       * TetrominoType::tetro_S
+       *
+       * ··█▉█▉··
+       * █▉█▉····
+       * ········
+       * ········
+       */
+      {{{1,1},{0, 1}, {0, 2}, {1, 0}}},
+
+      /*
+      * Z Tetromino
+      * TetrominoType::tetro_Z
+      *
+      * █▉█▉····
+      * ··█▉█▉··
+      * ········
+      * ········
+      */
+      {{{1,1},{0, 0}, {0, 1}, {1, 2}}},
+}};
+
+static constexpr BlockShape get_shape(TetrominoType type) noexcept {
+    return tetromino_shape[static_cast<int>(type) - 1];
+}
+
+Tetromino::Tetromino(TetrominoType type, const Axis& axis, BlockMap * blockMap) noexcept
+    : m_axis(axis),
+      m_blockmap(blockMap),
+      m_rotate_state(RotateState::angle_0)
+{
+    if(type == TetrominoType::empty){
+        // TODO: print warning info into log
+        type = TetrominoType::tetro_O;
+    }
+
+    m_type = type;
+    m_blocks = get_shape(type);
+
+}
+
+void Tetromino::fall() noexcept {
+    if(m_axis.x + 4 == block_map_properties.y_size){
+        // the tetromino is bottoming out
+        // notify the block map that the tetromino belongs to
+        m_blockmap -> bottom_out();
+    }
+
+    // bound check: check if the tetromino have space below
+    bool have_space_below = false;
+    for(Axis& block: m_blocks){
+        auto checked_x = m_axis.x + block.x + 1;
+        auto checked_y = m_axis.y + block.x;
+        if(m_blockmap -> get_block(checked_x, checked_y) != TetrominoType::empty){
+            have_space_below = true;
+            break;
+        }
+    }
+
+    if(have_space_below){
+        // the tetromino is bottoming out
+        // notify the block map that the tetromino belongs to
+        m_blockmap -> bottom_out();
+    } else {
+        // simply increase the y-axis by 1
+        ++m_axis.x;
+    }
+}
+
+void Tetromino::move_leftward() noexcept {
+    // bound check
+    if(m_axis.y == 0)
+        return;         // fail to move
+
+    // check if there is space on the left
+    bool have_space_on_left = true;
+    // traverse all blocks
+    for(Axis& block: m_blocks){
+        auto checked_x = m_axis.x + block.x;
+        auto checked_y = m_axis.y + block.y - 1;
+        if(m_blockmap -> get_block(checked_x, checked_y) != TetrominoType::empty){
+            have_space_on_left = false;
+            break;
+        }
+    }
+
+    if(have_space_on_left){
+        --m_axis.y;       // decrease the x-axis by 1
+    }
+    // otherwise, fail to move
+}
+
+void Tetromino::move_rightward() noexcept {
+    // bound check
+    if(m_axis.y + 4 == block_map_properties.x_size)
+        return;         // fail to move
+
+    // check if there is space on the right
+    bool have_space_on_right = true;
+    // traverse all blocks
+    for(Axis& block: m_blocks){
+        auto checked_x = m_axis.x + block.x;
+        auto checked_y = m_axis.y + block.y + 1;
+        if(m_blockmap -> get_block(checked_x, checked_y) != TetrominoType::empty){
+            have_space_on_right = false;
+            break;
+        }
+    }
+
+    if(have_space_on_right){
+        ++m_axis.y;       // decrease the y-axis by 1
+    }
+    // otherwise, fail to move
+}
+
+void Tetromino::rotate(bool clockwise) noexcept {
+    if(m_type == TetrominoType::tetro_O) return;
+
+    BlockShape destination;
+    bool is_rotation_blocked = false;
+
+    if(m_type == TetrominoType::tetro_I) {
+        eval_rotate_dest_I(m_blocks, destination, clockwise);
+    } else {
+        eval_rotate_dest_normal(m_blocks, destination, clockwise);
+    }
+    // traverse the block to check if any blocked
+    for(auto& dest_axis: destination) {
+        // if the rotation is
+        if(m_blockmap -> get_block(dest_axis) != TetrominoType::empty) {
+            is_rotation_blocked = true;
+            break;
+        }
+    }
+
+    // if the rotation is not blocked
+    if(!is_rotation_blocked) {
+        employ_rotation(destination, clockwise);
+        return;
+    }
+
+    // else: employ wall-kick movement
+    if(m_type == TetrominoType::tetro_I) {
+        // get wall-kick attempt list
+        auto& attempt_list = wall_kick_tetro_I(m_rotate_state,clockwise);
+        // traverse the attempt list
+        for(auto& displacement_axis: attempt_list) {
+            // check all
+            if(check_space_for_wall_kick(destination, displacement_axis)) {
+                // add wall-kick displacement to the destination
+                for(auto& axis: destination) {
+                    axis_displace(axis, displacement_axis);
+                }
+                // employ the rotation
+                employ_rotation(destination);
+            }
+        }
+
+        // otherwise, the rotation fails
+
+    } else {
+
+        // get wall-kick attempt list
+        auto& attempt_list = wall_kick_normal(m_rotate_state,clockwise);
+        // traverse the attempt list
+        for(auto& displacement_axis: attempt_list) {
+            // check all
+            if(check_space_for_wall_kick(destination, displacement_axis)) {
+                // add wall-kick displacement to the destination
+                for(auto& axis: destination) {
+                    axis_displace(axis, displacement_axis);
+                }
+                // employ the rotation
+                employ_rotation(destination);
+            }
+        }
+
+        // otherwise, the rotation fails
+
+    }
+}
+
+void Tetromino::change_rotate_state(RotateState &rotate_state, bool clockwise) noexcept {
+    if (clockwise) {
+        int target = static_cast<int>(rotate_state) + 1;
+        if(target >= 3) {
+            target = 0;
+        }
+        rotate_state = static_cast<RotateState>(target);
+        return;
+    }
+    else {
+        int target = static_cast<int>(rotate_state) - 1;
+        if(target <= 0) {
+            target = 3;
+        }
+        rotate_state = static_cast<RotateState>(target);
+        return;
+    }
+}
+
+void Tetromino::employ_rotation(const BlockShape &destination, bool clockwise) noexcept {
+    change_rotate_state(m_rotate_state, clockwise);
+    m_blocks = destination;
+}
+
+bool Tetromino::check_space_for_wall_kick(const BlockShape &origin_dest, const Axis& wall_kick_disp) const noexcept {
+    for(auto& axis: origin_dest) {
+        auto checked_x = axis.x + wall_kick_disp.x;
+        auto checked_y = axis.y + wall_kick_disp.y;
+
+        if(m_blockmap -> get_block(checked_x,checked_y) != TetrominoType::empty) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Tetromino::axis_displace(Axis &origin_axis, const Axis &displacement) {
+    origin_axis.x += displacement.x;
+    origin_axis.y += displacement.y;
+}
+
+
+
+
+
+
